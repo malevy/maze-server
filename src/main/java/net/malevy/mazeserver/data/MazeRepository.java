f2b194be2ba6de4c649b1aacccfb20e8b9e0d5d2
mike levy <michael.levy@techelevator.com>
2022-12-06 16:30:52 -0500

    add repo for maze storage

---
+++ src/main/java/net/malevy/mazeserver/data/MazeRepository.java
@@ -0,0 +1,29 @@
+package net.malevy.mazeserver.data;
+
+import net.malevy.mazeserver.creation.MazeBuilder;
+import net.malevy.mazeserver.domain.Maze;
+import net.malevy.mazeserver.domain.Size;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class MazeRepository {
+    final private Map<String, Maze> mazes = new HashMap<>();
+
+    public MazeRepository() {
+        final Maze maze = MazeBuilder.build(new Size(20,20));
+        mazes.put(maze.getId(), maze);
+    }
+
+    public Maze getById(String id) {
+        Assert.hasText(id, "must provide the ID");
+        return this.mazes.get(id);
+    }
+
+    public Maze[] getAll() {
+        return this.mazes.values().toArray(new Maze[0]);
+    }
+}
