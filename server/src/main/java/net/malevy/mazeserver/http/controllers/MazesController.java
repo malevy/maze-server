d212109674504ae28f85758454d00f8fedd655f6
mike levy <michael.levy@techelevator.com>
2022-12-09 13:11:55 -0500

    change port and enable CORS

--- server/src/main/java/net/malevy/mazeserver/http/controllers/MazesController.java
+++ server/src/main/java/net/malevy/mazeserver/http/controllers/MazesController.java
@@ -3,24 +3,16 @@ package net.malevy.mazeserver.http.controllers;
 import net.malevy.mazeserver.data.MazeRepository;
 import net.malevy.mazeserver.domain.Cell;
 import net.malevy.mazeserver.domain.Maze;
-import net.malevy.mazeserver.http.Constants;
 import net.malevy.mazeserver.http.ResponseFactory;
-import net.malevy.mazeserver.http.dtos.CollectionDto;
-import net.malevy.mazeserver.http.dtos.ItemDto;
-import net.malevy.mazeserver.http.dtos.LinkDto;
 import net.malevy.mazeserver.http.dtos.MazeDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @RestController
 @RequestMapping("/mazes")
+@CrossOrigin
 public class MazesController {
 
     final private MazeRepository mazeRepository;
