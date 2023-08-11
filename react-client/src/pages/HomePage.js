deeafcc847f9aa39a0510e8b7e8109c91454cb8d
mike levy <michael.levy@techelevator.com>
2023-08-11 10:00:46 -0400

    remove useEffect

--- react-client/src/pages/HomePage.js
+++ react-client/src/pages/HomePage.js
@@ -5,7 +5,6 @@ import { useStore } from "../contexts/Store";
 
 function HomePage() {
   const [mazes, setMazes] = React.useState([]);
-  const [selectedMaze, setSelectedMaze] = React.useState(null);
   const { storeNavigation } = useStore();
 
   React.useEffect(() => {
@@ -17,20 +16,17 @@ function HomePage() {
     loadMazes();
   }, []);
 
-  React.useEffect(() => {
-    async function enterMaze() {
-      const mazeStart = await mazeServer.getStartUrl(selectedMaze.href);
-      const startCell = await mazeServer.goToCell(mazeStart.href);
-      storeNavigation(startCell, "south");
-    }
-    if (selectedMaze) enterMaze();
-  }, [selectedMaze, storeNavigation]);
+  async function enterMaze(maze) {
+    const mazeStart = await mazeServer.getStartUrl(maze.href);
+    const startCell = await mazeServer.goToCell(mazeStart.href);
+    storeNavigation(startCell, "south");
+  }
 
   function mazeToRow(maze) {
     return (
       <tr key={maze.href}>
         <td>
-          <button className="link-button" onClick={() => setSelectedMaze(maze)}>
+          <button className="link-button" onClick={() => enterMaze(maze)}>
             {maze.name}
           </button>
         </td>
